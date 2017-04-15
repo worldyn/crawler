e46cbf5e1e90dc977c400736da883eca55e4ddd4
Adam J <adamjac@kth.se>
2017-04-15 17:51:26 +0200

    get request to db done

--- server.go
+++ server.go
@@ -6,17 +6,17 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"net/http"
-	"html"
 	"log"
-	//"encoding/json"
+	"encoding/json"
 	"gopkg.in/mgo.v2"
 	"time"
-  //"gopkg.in/mgo.v2/bson"
-	//"sync"
+  "gopkg.in/mgo.v2/bson"
 )
 
 func main() {
-	// MONGO SETUP
+	/*
+	* MONGO SETUP
+	*/
 	session, conErr := mgo.Dial("127.0.0.1:27017")
 	if conErr != nil {
 		fmt.Println("ERROR: mongo connection error")
@@ -29,69 +29,114 @@ func main() {
 
 	session.SetMode(mgo.Monotonic, true)
 
-	// Collection People
-	c := session.DB("crawler").C("listings")
+	ensureIndex(session)
 
-	// Index
-	index := mgo.Index{
-		Key:        []string{"ListingLink"},
-		Unique:     true,
-		DropDups:   true,
-		Background: true,
-		Sparse:     true,
-	}
+	/*
+	* Concurrent Database Updating
+	*/
 
-	indexErr := c.EnsureIndex(index)
-	if indexErr != nil {
-		fmt.Println("ERROR: mongo index error")
-		fmt.Println("MSG:", indexErr.Error())
-		panic(indexErr)
-	}
-
-	// CRAWLING DATABASE Updating
 	go func() {
 		for {
-			<-time.After(20 * time.Second)
-			chListings := make(chan scrape.Listing)
-			chDone := make(chan bool)
-
-			urlCount := 1
-
-			akScraper := scrapeImplementations.AkKvartScraper { "http://akademiskkvart.se", "/?limit=500" }
-			go scrape.ParseAndScrape(akScraper, chListings, chDone)
-
-			for crawlersDone := 0; crawlersDone < urlCount; {
-				select {
-				case listing := <-chListings:
-					insertErr := c.Insert(listing)
-					//fmt.Println(json.Marshal(&listing))
-
-					if insertErr != nil &&  !mgo.IsDup(insertErr) {
-						fmt.Println("ERROR: mongo insert error")
-						fmt.Println("MSG:", insertErr.Error())
-						panic(insertErr)
-					}
-
-				case <-chDone:
-					crawlersDone++
-				}
-			}
-			fmt.Println("Mongodb insertion done...")
+			updateWaiter()
+			update(session)
 		}
 	}()
 
-	// REST API path
+	/*
+	* REST API logic
+	*/
 	router := mux.NewRouter().StrictSlash(true)
-  router.HandleFunc("/", GetListings)
+  router.HandleFunc("/", getListings(session))
   log.Fatal(http.ListenAndServe(":8080", router))
-
 }
 
-func GetListings(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+// MongoDB setup settings
+func ensureIndex(s *mgo.Session) {
+    session := s.Copy()
+    defer session.Close()
+
+    c := session.DB("crawler").C("listings")
+
+		index := mgo.Index{
+			Key:        []string{"ListingLink"},
+			Unique:     true,
+			DropDups:   true,
+			Background: true,
+			Sparse:     true,
+		}
+
+		indexErr := c.EnsureIndex(index)
+		if indexErr != nil {
+			fmt.Println("ERROR: mongo index error")
+			fmt.Println("MSG:", indexErr.Error())
+			panic(indexErr)
+		}
 }
 
-/*func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
+// Wait a certain amount of time. Decides how often the db will update
+func updateWaiter() {
+	<-time.After(20 * time.Second)
+}
+
+// update mongo db by scraping all the house listings
+func update(s *mgo.Session) {
+	session := s.Copy()
+	defer session.Close()
+	c := session.DB("crawler").C("listings")
+
+	chListings := make(chan scrape.Listing)
+	chDone := make(chan bool)
+
+	urlCount := 1
+
+	akScraper := scrapeImplementations.AkKvartScraper { "http://akademiskkvart.se", "/?limit=500" }
+	go scrape.ParseAndScrape(akScraper, chListings, chDone)
+
+	for crawlersDone := 0; crawlersDone < urlCount; {
+		select {
+		case listing := <-chListings:
+			insertErr := c.Insert(listing)
+			//fmt.Println(json.Marshal(&listing))
+
+			if insertErr != nil &&  !mgo.IsDup(insertErr) {
+				fmt.Println("ERROR: mongo insert error")
+				fmt.Println("MSG:", insertErr.Error())
+				panic(insertErr)
+			}
+
+		case <-chDone:
+			crawlersDone++
+		}
+	}
+	fmt.Println("Mongodb insertion done...")
+}
+
+// GET request where you get listings from mongo database
+func getListings(s *mgo.Session) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		session := s.Copy()
+		defer session.Close()
+		c := session.DB("crawler").C("listings")
+
+		var listings []scrape.Listing
+		findErr := c.Find(bson.M{}).All(&listings)
+		if findErr != nil {
+		  ErrorWithJSON(w, "Database error", http.StatusInternalServerError)
+		  log.Println("Failed get all books: ", findErr)
+		  return
+		}
+
+		respBody, respErr := json.MarshalIndent(listings, "", "  ")
+		if respErr != nil {
+		  log.Fatal(respErr)
+		}
+
+		ResponseWithJSON(w, respBody, http.StatusOK)
+	}
+}
+
+// Api response logic
+func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
     w.Header().Set("Content-Type", "application/json; charset=utf-8")
     w.WriteHeader(code)
     fmt.Fprintf(w, "{message: %q}", message)
@@ -101,4 +146,4 @@ func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
     w.Header().Set("Content-Type", "application/json; charset=utf-8")
     w.WriteHeader(code)
     w.Write(json)
-}*/
+}
