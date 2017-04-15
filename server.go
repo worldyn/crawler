87187598dae4dde2a3e889489c6a70239909f48c
Adam J <adamjac@kth.se>
2017-04-15 17:24:53 +0200

    added basic REST path and concurrent database uodating

--- server.go
+++ server.go
@@ -4,10 +4,13 @@ import (
 	"crawler/scrape"
 	"crawler/scrapeImplementations"
 	"fmt"
-	//"github.com/gorilla/mux"
-	//"net/http"
+	"github.com/gorilla/mux"
+	"net/http"
+	"html"
+	"log"
 	//"encoding/json"
 	"gopkg.in/mgo.v2"
+	"time"
   //"gopkg.in/mgo.v2/bson"
 	//"sync"
 )
@@ -45,35 +48,57 @@ func main() {
 		panic(indexErr)
 	}
 
-	// CRAWLING
-	chListings := make(chan scrape.Listing)
-	chDone := make(chan bool)
+	// CRAWLING DATABASE Updating
+	go func() {
+		for {
+			<-time.After(20 * time.Second)
+			chListings := make(chan scrape.Listing)
+			chDone := make(chan bool)
 
-	//tmp
-	urlCount := 1
+			urlCount := 1
 
-	akScraper := scrapeImplementations.AkKvartScraper { "http://akademiskkvart.se", "/?limit=500" }
-	go scrape.ParseAndScrape(akScraper, chListings, chDone)
+			akScraper := scrapeImplementations.AkKvartScraper { "http://akademiskkvart.se", "/?limit=500" }
+			go scrape.ParseAndScrape(akScraper, chListings, chDone)
 
-	for crawlersDone := 0; crawlersDone < urlCount; {
-		select {
-		case listing := <-chListings:
-			insertErr := c.Insert(listing)
-			//fmt.Println(json.Marshal(&listing))
+			for crawlersDone := 0; crawlersDone < urlCount; {
+				select {
+				case listing := <-chListings:
+					insertErr := c.Insert(listing)
+					//fmt.Println(json.Marshal(&listing))
 
-			if insertErr != nil &&  !mgo.IsDup(insertErr) {
-				fmt.Println("ERROR: mongo insert error")
-				fmt.Println("MSG:", insertErr.Error())
-				panic(insertErr)
+					if insertErr != nil &&  !mgo.IsDup(insertErr) {
+						fmt.Println("ERROR: mongo insert error")
+						fmt.Println("MSG:", insertErr.Error())
+						panic(insertErr)
+					}
+
+				case <-chDone:
+					crawlersDone++
+				}
 			}
-
-		case <-chDone:
-			crawlersDone++
+			fmt.Println("Mongodb insertion done...")
 		}
-	}
-	fmt.Println("Mongodb insertion done...")
+	}()
 
-
-
+	// REST API path
+	router := mux.NewRouter().StrictSlash(true)
+  router.HandleFunc("/", GetListings)
+  log.Fatal(http.ListenAndServe(":8080", router))
 
 }
+
+func GetListings(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+}
+
+/*func ErrorWithJSON(w http.ResponseWriter, message string, code int) {
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    w.WriteHeader(code)
+    fmt.Fprintf(w, "{message: %q}", message)
+}
+
+func ResponseWithJSON(w http.ResponseWriter, json []byte, code int) {
+    w.Header().Set("Content-Type", "application/json; charset=utf-8")
+    w.WriteHeader(code)
+    w.Write(json)
+}*/
